0c8029df06fc824463d410352cde8e9d89228269
TM <575435@cognizant.com>
2018-03-28 13:34:53 +0530

    Added Products component and Custom pipe and service

--- src/Products/product-list.component.ts
+++ src/Products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./prouct.service";
 
 @Component({
     selector: `pm-products`,
@@ -10,8 +11,12 @@ import { IProduct } from "./product";
 export class ProductListComponent implements OnInit{
     //productList = [{'name':'Mi A1','description':'Smartphone'}, {'name':'Dell','description':'Laptop'}];
     filteredProducts : IProduct[];
+
+    constructor(private _productServie: ProductService) {
+        
+    }
+
     pageTitle: string = 'Product List';
-    //listFilter: string = 'cart';
     showImage: boolean = false;
 
     _listFilter: string;
@@ -25,31 +30,9 @@ export class ProductListComponent implements OnInit{
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
 
-    products: IProduct[] =  [{
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    },
-    {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-    }];
 
-    constructor(){
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
-    }
+    products: IProduct[] ;
+
 
     performFilter(filterBy: string): IProduct[]{
         filterBy = filterBy.toLocaleLowerCase();
@@ -62,6 +45,7 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit():void{
+        this.products = this._productServie.getProducts();
         console.log('In OnInit');
     }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,20 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from '../Products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { ProductService } from '../Products/prouct.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe    
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
